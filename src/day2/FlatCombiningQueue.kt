465603ade4e805db0d1fa81da6aa3b703e7b43e8
AndreyYurko <andreyyurko01@gmail.com>
2023-05-31 18:30:51 +0300

    first and second day done'

--- src/day2/FlatCombiningQueue.kt
+++ src/day2/FlatCombiningQueue.kt
@@ -21,7 +21,70 @@ class FlatCombiningQueue<E> : Queue<E> {
         // TODO:      `null` to the element. Wait until either the cell state
         // TODO:      updates to `PROCESSED` (do not forget to clean it in this case),
         // TODO:      or `combinerLock` becomes available to acquire.
-        queue.addLast(element)
+        // we need to either get lock or get cell
+        while (true) {
+            if (combinerLock.compareAndSet(expect = false, update = true)) {
+                // got lock, perform adding
+                queue.addLast(element)
+                // help others
+                help()
+                // release lock
+                combinerLock.compareAndSet(true, update = false)
+                return
+            } else {
+                val index = randomCellIndex()
+                // create operation
+                val operation = Operation(element)
+                // try to get cell
+                if (tasksForCombiner[index].compareAndSet(null, operation)) {
+                    // now this is out cell - we need to release it
+                    while (true) {
+                        // got lock
+                        if (combinerLock.compareAndSet(expect = false, update = true)) {
+                            // get current operation status
+                            val operationStatus = tasksForCombiner[index].value as Operation
+                            // if nobody performed operation we need to perform it
+                            if (operationStatus.state == null) queue.addLast(element)
+                            // release the cell and help others
+                            tasksForCombiner[index].compareAndSet(operationStatus, null)
+
+                            help()
+                            combinerLock.compareAndSet(true, update = false)
+                            return
+                        } else {
+                            // we didn't get lock
+                            // so let's check our progress in cell
+                            val curValue = tasksForCombiner[index].value as Operation
+                            if (curValue.state == PROCESSED) {
+                                // if state is "PROCESSED" only we can change it to null
+                                tasksForCombiner[index].getAndSet(null)
+                                return
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun help() {
+        // for each cell in array
+        for (i in 0 until tasksForCombiner.size) {
+            val cell = tasksForCombiner[i]
+            val operation = cell.value as Operation?
+            if (operation == null) continue
+            // nobody can change state of operation except combiner
+            if (operation.state == PROCESSED) continue
+            if (operation.task == DEQUE_TASK) {
+                val completedOperation = Operation(queue.removeFirstOrNull(), PROCESSED)
+                cell.getAndSet(completedOperation)
+            }
+            else {
+                queue.addLast(operation.task as E)
+                val completedOperation = Operation(operation.task, PROCESSED)
+                cell.getAndSet(completedOperation)
+            }
+        }
     }
 
     override fun dequeue(): E? {
@@ -36,11 +99,53 @@ class FlatCombiningQueue<E> : Queue<E> {
         // TODO:      `null` to `DEQUE_TASK`. Wait until either the cell state
         // TODO:      updates to `PROCESSED` (do not forget to clean it in this case),
         // TODO:      or `combinerLock` becomes available to acquire.
-        return queue.removeFirstOrNull()
+        while (true) {
+            if (combinerLock.compareAndSet(expect = false, update = true)) {
+                // as combiner we have all rights to get value
+                // after perform regular operations
+                val value = queue.removeFirstOrNull()
+                help()
+                combinerLock.compareAndSet(true, update = false)
+                return value
+            } else {
+                // try to get cell
+                val index = randomCellIndex()
+                val operation = Operation(DEQUE_TASK)
+                if (tasksForCombiner[index].compareAndSet(null, operation)) {
+                    while (true) {
+                        // we need to free this cell
+                        if (combinerLock.compareAndSet(expect = false, update = true)) {
+                            // we are combiner now
+                            // check if other not performed operation
+                            val curValue = tasksForCombiner[index].value as Operation
+                            val returnedValue = if (curValue.state == null) queue.removeFirstOrNull() else curValue.task as E?
+                            // release cell
+                            tasksForCombiner[index].getAndSet(null)
+
+                            help()
+                            combinerLock.compareAndSet(true, update = false)
+                            return returnedValue
+                        } else {
+                            // check our progress
+                            val curValue = tasksForCombiner[index].value as Operation
+                            if (curValue.state == PROCESSED) {
+                                tasksForCombiner[index].getAndSet(null)
+                                return curValue.task as E?
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private fun randomCellIndex(): Int =
         ThreadLocalRandom.current().nextInt(tasksForCombiner.size)
+
+    private data class Operation(
+        val task: Any?,
+        val state: Any? = null
+    )
 }
 
 private const val TASKS_FOR_COMBINER_SIZE = 3 // Do not change this constant!
